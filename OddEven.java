37161740dd54301619bc81705a2f0433c288dec9
gulatikeshav <133638164+gulatikeshav@users.noreply.github.com>
2023-10-06 16:49:45 +0530

    Update OddEven.java

--- OddEven.java
+++ OddEven.java
@@ -1,11 +1,20 @@
-public class OddEven {
+import java.util.*;
+
+public class oddEvenChecker {
+
     public static void main(String args[]) {
-        int n = 10;
-        int bitMask = 1;
-        if((n & bitMask) == 0) {
-            System.out.println("even");
-        } else {
-            System.out.println("odd");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if(num % 2 == 0){
+            System.out.println(num + " is even");
         }
+        else{
+            System.out.println(num + " is odd");
+        }
+        sc.close();
     }
 }
